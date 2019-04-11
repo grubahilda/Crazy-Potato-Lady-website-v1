2e147b9d72723f1aca154a29b4d3f59ff7d8733e
grubahilda <marta.pozaroszczyk@planipy.com>
2019-04-11 12:44:08 +0200

    Add first version of the home page

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO:
+
+- correct fourth div, the picture-text alignment
+- correct top animations' position
+- add crossfade for slideshow
--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 // NAVBAR RESPONSIVE COLLAPSE
 function navbarCollapse() {
     let x = document.getElementById("topNav");
@@ -28,15 +26,28 @@ function showDivs(n) {
     var images = document.getElementsByClassName("recipeSlides");
     var dots = document.getElementsByClassName("demo");
 
-    if(n > images.length) {slideIndex = 1}
-    if(n < 1) {slideIndex = images.length}
+    if (n > images.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = images.length
+    }
     for (var i = 0; i < images.length; i++) {
         images[i].classList.remove("active");
     }
     for (var i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" w3-white", "");
     }
-    images[slideIndex-1].className += " active";
+    images[slideIndex - 1].className += " active";
     // images[slideIndex-1].className += "showMe";
-    dots[slideIndex-1].className += " w3-white";
+    dots[slideIndex - 1].className += " w3-white";
 }
+
+
+
+
+// Footnote copyright year
+window.onload = function() {
+    d = new Date();
+    document.getElementById("currentYear").innerHTML = d.getFullYear();
+};
