f29e99486bff3f9c81fa494365e080e989affb38
grubahilda <marta.pozaroszczyk@planipy.com>
2019-04-24 20:25:41 +0200

    Add carousel for recipes, not working yet

--- js/index.js
+++ js/index.js
@@ -59,3 +59,22 @@ window.onload = function() {
 window.onload = function() {
     document.getElementsByTagName("footer")[0].style.backgroundColor = document.getElementsByTagName("body")[0].style.backgroundColor;
 }
+
+
+
+$('.center').slick({
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500,
+    variableWidth: false,
+  });
+  $('.center').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    console.log('beforeChange', currentSlide, nextSlide);
+  });
+  $('.center').on('afterChange', function(event, slick, currentSlide){
+    console.log('afterChange', currentSlide);
+  });
+
+
