3affc1e419336085d7e28f2bd0a7755063557847
grubahilda <marta.pozaroszczyk@planipy.com>
2019-06-03 13:47:38 +0200

    Add div one index page responsivenes + owl carousel recipes

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,12 @@ function navbarCollapse() {
 }
 
 
+//GLOBALS
+var IngredientId = 1;
+var slideIndex = 1;
+
 // RECIPES SLIDE SHOW
 
-var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -54,7 +57,7 @@ function showDivs(n) {
 window.onload = function () {
     d = new Date();
     document.getElementById("currentYear").innerHTML = d.getFullYear();
-};
+}
 
 
 
@@ -85,10 +88,100 @@ function mouseUpPhoneNumber() {
 // Touch events on slide show index.ejs
 
 
-var sliderMain = document.getElementById('sliderMain');
+// var sliderMain = document.getElementById('sliderMain');
 
-var hammertime = new Hammer(sliderMain);
+// var hammertime = new Hammer(sliderMain);
 
-hammertime.on("swipe", function(ev){
+// hammertime.on("swipe", function(ev){
     
-});
+// });
+
+$(document).ready(function() {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      margin: 0,
+      nav: true,
+      loop: false,
+      responsive: {
+        0: {
+            items: 1
+          },
+        600: {
+            items: 2
+          },
+        950: {
+          items: 3
+        },
+        1250: {
+          items: 4
+        },
+        1500: {
+          items: 5
+        }
+      }
+    })
+  })
+
+
+function addIngredient() {
+
+    console.log(document.getElementById("fieldIngredients").children.length-1);
+    
+    if(document.getElementById("fieldIngredients").childElementCount > 2) {
+        document.getElementById("ingredient" + IngredientId).removeChild(document.getElementsByClassName("addIngredientButton")[0]);
+    }
+ 
+    IngredientId++;
+    
+    const newIngredientDiv = document.createElement("div");
+    newIngredientDiv.id = "ingredient" + IngredientId;
+    // newIngredientDiv.className = "";
+    document.getElementById("fieldIngredients").appendChild(newIngredientDiv);
+    const newIngredientName = document.createElement("input");
+    newIngredientName.type = "text";
+    newIngredientName.placeholder = "Ingredient";
+    document.getElementById(newIngredientDiv.id).appendChild(newIngredientName);
+    const newIngredientAmount = document.createElement("input");
+    newIngredientAmount.type = "text";
+    newIngredientAmount.placeholder = "Amount";
+    document.getElementById(newIngredientDiv.id).appendChild(newIngredientAmount);
+    const newIngredientSelect = document.createElement("select");
+    document.getElementById(newIngredientDiv.id).appendChild(newIngredientSelect);
+    const newIngredientOptionPiece = document.createElement("option");
+    newIngredientOptionPiece.value = "piece";
+    newIngredientOptionPiece.text = "Piece";
+    document.getElementById(newIngredientDiv.id).getElementsByTagName("select")[0].appendChild(newIngredientOptionPiece);
+    const newIngredientOptionMl = document.createElement("option");
+    newIngredientOptionMl.value = "ml";
+    newIngredientOptionMl.text = "ml";
+    document.getElementById(newIngredientDiv.id).getElementsByTagName("select")[0].appendChild(newIngredientOptionMl);
+    const newIngredientOptionG = document.createElement("option");
+    newIngredientOptionG.value = "g";
+    newIngredientOptionG.text = "g";
+    document.getElementById(newIngredientDiv.id).getElementsByTagName("select")[0].appendChild(newIngredientOptionG);
+    const newIngredientButton = document.createElement("button");
+    newIngredientButton.type = "button";
+    newIngredientButton.className = "addIngredientButton";
+    newIngredientButton.setAttribute("onClick", "addIngredient()");
+    newIngredientButton.innerText = "+";
+    document.getElementById(newIngredientDiv.id).appendChild(newIngredientButton);
+    const IngredientButtonRemove = document.createElement("button");
+    IngredientButtonRemove.type = "button";
+    IngredientButtonRemove.className = "removeIngredientButton";
+    IngredientButtonRemove.setAttribute("onClick", "removeIngredient(" + newIngredientDiv.id + ")");
+    IngredientButtonRemove.innerText = "-";
+    document.getElementById(newIngredientDiv.id).appendChild(IngredientButtonRemove);
+
+}
+
+function removeIngredient(ingredient) {
+    
+    document.getElementById("fieldIngredients").removeChild(ingredient);
+
+    const newIngredientButton = document.createElement("button");
+    newIngredientButton.type = "button";
+    newIngredientButton.className = "addIngredientButton";
+    newIngredientButton.setAttribute("onClick", "addIngredient()");
+    newIngredientButton.innerText = "+";
+    document.getElementById("fieldIngredients").lastElementChild.appendChild(newIngredientButton);
+}
